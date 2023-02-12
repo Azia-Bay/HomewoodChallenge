9b4f713332b0e1b38d56993cb1aeaf8c1130dd39
Azia Bay-Asen <aziabayasen@gmail.com>
2023-02-11 19:18:06 -0600

    Implemented gallery

---
+++ HomewoodChallenge/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+using HomewoodChallenge.Views;
+
+[assembly: ExportFont("Lato-Bold.ttf", Alias = "Lato")]
+namespace HomewoodChallenge
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new GalleryPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ HomewoodChallenge/Views/GalleryPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Reflection;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace HomewoodChallenge.Views
+{
+    public class FavoritableImage
+    {
+        public ImageSource Source { get; }
+        public bool Favorited = false;
+
+        public FavoritableImage(ImageSource source) => Source = source;
+    }
+
+    public partial class GalleryPage : ContentPage
+    {
+        private static readonly string _imageFilePath = "HomewoodChallenge.Resources.Images.";
+
+        public ObservableCollection<FavoritableImage> ImageCollection { get; } = new ObservableCollection<FavoritableImage>()
+        {
+            new FavoritableImage(ImageSource.FromResource(_imageFilePath + "albert-cashier.jpg")),
+            new FavoritableImage(ImageSource.FromResource(_imageFilePath + "black-gay-man.jpg")),
+            new FavoritableImage(ImageSource.FromResource(_imageFilePath + "boy-attacked-by-police-dog.jpg")),
+            new FavoritableImage(ImageSource.FromResource(_imageFilePath + "desegregation-protest.jpg")),
+            new FavoritableImage(ImageSource.FromResource(_imageFilePath + "emmett-till-casket.png")),
+            new FavoritableImage(ImageSource.FromResource(_imageFilePath + "harvey-milk-against-briggs-initiative.jpg")),
+            new FavoritableImage(ImageSource.FromResource(_imageFilePath + "i-am-a-man.jpg")),
+            new FavoritableImage(ImageSource.FromResource(_imageFilePath + "leslie-feinberg-rally.jpg")),
+            new FavoritableImage(ImageSource.FromResource(_imageFilePath + "leslie-feinberg-transexual-menace.jpg")),
+            new FavoritableImage(ImageSource.FromResource(_imageFilePath + "little-rock-nine.jpg")),
+            new FavoritableImage(ImageSource.FromResource(_imageFilePath + "margaret-hamilton.jpeg")),
+            new FavoritableImage(ImageSource.FromResource(_imageFilePath + "mlk-i-have-a-dream.jpeg")),
+            new FavoritableImage(ImageSource.FromResource(_imageFilePath + "napalm-girl.jpg")),
+            new FavoritableImage(ImageSource.FromResource(_imageFilePath + "olympics-black-power-salute.jpeg")),
+            new FavoritableImage(ImageSource.FromResource(_imageFilePath + "voting-rights-protest.jpg")),
+            new FavoritableImage(ImageSource.FromResource(_imageFilePath + "white-only-water-fountain.jpg")),
+            new FavoritableImage(ImageSource.FromResource(_imageFilePath + "woolsworth-sit-in.png")),
+            new FavoritableImage(ImageSource.FromResource(_imageFilePath + "workers-atop-a-skyscraper.jpeg"))
+
+        };
+        public int Columns { get; } = 3;
+        public int ImageSpacing { get; } = 5;
+        // , typeof(GalleryPage).GetTypeInfo().Assembly)
+
+        private int GetImageSize()
+            => (int)(DeviceDisplay.MainDisplayInfo.Width / DeviceDisplay.MainDisplayInfo.Density 
+                - ImageSpacing * Columns - ImageSpacing) / Columns;
+
+        public int ImageSize => GetImageSize();
+
+        public GalleryPage()
+        {
+            DeviceDisplay.MainDisplayInfoChanged += DeviceDisplay_MainDisplayInfoChanged;
+
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        private void DeviceDisplay_MainDisplayInfoChanged(object sender, DisplayInfoChangedEventArgs e)
+            => OnPropertyChanged("ImageSize");
+    }
+}
